889c7ced7ea732cdfe82f467516a189dc44ec53c
David Rynearson <david.rynearson@sva.de>
2020-04-23 15:20:01 +0200

    Code cleanup, added logger

--- index.js
+++ index.js
@@ -1,139 +1,132 @@
-var request = require('request');
-const YAML = require('yaml');
+const request = require('request');
+const path = require('path');
 const fs = require('fs');
+const pino = require('pino');
 
-var apiBaseUrl = 'https://api.netfield.io'
-var apiVersion = 'v1'
-
-async function convertYamlData(filePath) {
-    const file = fs.readFileSync(__dirname + filePath, 'utf8');
-    var data = YAML.parse(file);
-    return data;
-}
+const apiBaseUrl = 'https://api.netfield.io';
+const apiVersion = 'v1';
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
 
 function checkForRightResponse(response) {
-    if (response.statusCode >= 400) {
-        // throw new Error(`Connection Error. Status Code ${response.statusCode}`);
-        // Best practise seems to be 1) use process.exitCode = 1, 2) throw uncaught error
-        console.log(`An Error occured with the Status Code: ${response.statusCode}`)
-        process.exit(1);
-    }
+  if (response.statusCode >= 400) {
+    // throw new Error(`Connection Error. Status Code ${response.statusCode}`);
+    // Best practise seems to be 1) use process.exitCode = 1, 2) throw uncaught error
+    logger.info(`An Error occured with the Status Code: ${response.statusCode}`);
+    process.exit(1);
+  }
 }
 
+
 function createFormdataObject(ContainerCreateOptions) {
-    returnObject = {
-        // mqttTopics not included because it needs to be a list with at least 1 items and cannot be empty
-        'displayName': ContainerCreateOptions.displayName,
-        'containerName': ContainerCreateOptions.displayName.toLowerCase().replace(/ /g, "-"),
-        'version': ContainerCreateOptions.version,
-        'restartPolicy': ContainerCreateOptions.restartPolicy,
-        'type': ContainerCreateOptions.type,
-        'desiredStatus': ContainerCreateOptions.desiredStatus,
-        'imageUri': `${ContainerCreateOptions.imageUri}:${ContainerCreateOptions.tagName}`,
-        'category': ContainerCreateOptions.category,
-        'processorArchitecture': ContainerCreateOptions.processorArchitecture,
-        'shortDescription': ContainerCreateOptions.shortDescription,
-        'description': ContainerCreateOptions.description,
-        'containerCreateOptions': JSON.stringify(ContainerCreateOptions.containerCreateOptions),
-        'containerTwinDesiredOptions': JSON.stringify(ContainerCreateOptions.containerTwinDesiredOptions),
-        'environmentVariables': JSON.stringify(ContainerCreateOptions.environmentVariables),
-        'containerType': ContainerCreateOptions.containerType,
-        'organisationId': ContainerCreateOptions.organisationId,
-        'registryType': ContainerCreateOptions.registryType,
-        'credentials': JSON.stringify(ContainerCreateOptions.credentials)
-    };
-    return returnObject;
+  return {
+    // mqttTopics not included because it needs to be a list with at least 1
+    // items and cannot be empty
+    displayName: ContainerCreateOptions.displayName,
+    containerName: ContainerCreateOptions.displayName.toLowerCase().replace(/ /g, '-'),
+    version: ContainerCreateOptions.version,
+    restartPolicy: ContainerCreateOptions.restartPolicy,
+    type: ContainerCreateOptions.type,
+    desiredStatus: ContainerCreateOptions.desiredStatus,
+    imageUri: `${ContainerCreateOptions.imageUri}:${ContainerCreateOptions.tagName}`,
+    category: ContainerCreateOptions.category,
+    processorArchitecture: ContainerCreateOptions.processorArchitecture,
+    shortDescription: ContainerCreateOptions.shortDescription,
+    description: ContainerCreateOptions.description,
+    containerCreateOptions: JSON.stringify(ContainerCreateOptions.containerCreateOptions),
+    containerTwinDesiredOptions: JSON.stringify(ContainerCreateOptions.containerTwinDesiredOptions),
+    environmentVariables: JSON.stringify(ContainerCreateOptions.environmentVariables),
+    containerType: ContainerCreateOptions.containerType,
+    organisationId: ContainerCreateOptions.organisationId,
+    registryType: ContainerCreateOptions.registryType,
+    credentials: JSON.stringify(ContainerCreateOptions.credentials),
+  };
 }
 
 module.exports = {
-    updateContainer: function (apiKey, containerId, ContainerCreateOptions) {
-        return new Promise(function (resolve, reject) {
-            var options = {
-                'method': 'PUT',
-                'url': `${apiBaseUrl}/${apiVersion}/containers/${containerId}`,
-                'headers': {
-                    'Authorization': apiKey
-                },
-                formData: createFormdataObject(ContainerCreateOptions)
-            };
-            // console.log(options);
-            request(options, function (error, response) {
-                if (error) throw new Error(error);
-                console.log('Updated Container Body:');
-                console.log(response.body);
-                checkForRightResponse(response);
-                resolve(response.body);
-            });
-        });
-    },
+  updateContainer(apiKey, containerId, ContainerCreateOptions) {
+    return new Promise(((resolve, reject) => {
+      const options = {
+        method: 'PUT',
+        url: `${apiBaseUrl}/${apiVersion}/containers/${containerId}`,
+        headers: {
+          Authorization: apiKey,
+        },
+        formData: createFormdataObject(ContainerCreateOptions),
+      };
+      // logger.info(options);
+      request(options, (error, response) => {
+        if (error) throw reject(new Error(error));
+        logger.info('Updated Container Body:');
+        logger.info(response.body);
+        checkForRightResponse(response);
+        resolve(response.body);
+      });
+    }));
+  },
 
-    deleteDeviceContainer: function (apiKey, deviceId, containerId) {
-        return new Promise(function (resolve, reject) {
-            var options = {
-                'method': 'DELETE',
-                'url': `${apiBaseUrl}/${apiVersion}/devices/${deviceId}/containers/${containerId}`,
-                'headers': {
-                    'Authorization': apiKey,
-                    'Content-Type': 'application/json'
-                }
-            };
-            request(options, function (error, response) {
-                if (error) throw new Error(error);
-                console.log('Delete Device Container Body:');
-                console.log(response.body);
-                resolve(response.body);
-            });
-        });
-    },
+  deleteDeviceContainer(apiKey, deviceId, containerId) {
+    return new Promise(((resolve, reject) => {
+      const options = {
+        method: 'DELETE',
+        url: `${apiBaseUrl}/${apiVersion}/devices/${deviceId}/containers/${containerId}`,
+        headers: {
+          Authorization: apiKey,
+          'Content-Type': 'application/json',
+        },
+      };
+      request(options, (error, response) => {
+        if (error) throw reject(new Error(error));
+        logger.info('Delete Device Container Body:');
+        logger.info(response.body);
+        resolve(response.body);
+      });
+    }));
+  },
 
-    createDeviceContainer: function (apiKey, deviceId, containerId, containerOption = '{}') {
-        return new Promise(function (resolve, reject) {
-            var options = {
-                'method': 'POST',
-                'url': `${apiBaseUrl}/${apiVersion}/devices/${deviceId}/containers/${containerId}`,
-                'headers': {
-                    'Content-Type': 'application/json',
-                    'Authorization': apiKey
-                },
-                body: containerOption
+  createDeviceContainer(apiKey, deviceId, containerId, containerOption = '{}') {
+    return new Promise(((resolve, reject) => {
+      const options = {
+        method: 'POST',
+        url: `${apiBaseUrl}/${apiVersion}/devices/${deviceId}/containers/${containerId}`,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: apiKey,
+        },
+        body: containerOption,
 
-            };
-            // console.log(options);
-            request(options, function (error, response) {
-                if (error) throw new Error(error);
-                console.log('Create Device Container Body:');
-                console.log(response.body);
-                checkForRightResponse(response);
-                resolve(response.body);
-            });
-        });
-    },
+      };
+      request(options, (error, response) => {
+        if (error) throw reject(new Error(error));
+        logger.info('Create Device Container Body:');
+        logger.info(response.body);
+        checkForRightResponse(response);
+        resolve(response.body);
+      });
+    }));
+  },
 
-    createContainer: function (apiKey, ContainerCreateOptions) {
-        return new Promise(function (resolve, reject) {
-            var options = {
-                'method': 'POST',
-                'url': `${apiBaseUrl}/${apiVersion}/containers`,
-                'headers': {
-                    'Authorization': apiKey,
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                formData: createFormdataObject(ContainerCreateOptions)
-            };
-            // console.log(options);
-            request(options, function (error, response) {
-                if (error) throw new Error(error);
-                console.log('Create Container Body:');
-                console.log(response.body);
-                checkForRightResponse(response);
-                resolve(response.body);
-            });
-        });
-    },
+  createContainer(apiKey, ContainerCreateOptions) {
+    return new Promise(((resolve, reject) => {
+      const options = {
+        method: 'POST',
+        url: `${apiBaseUrl}/${apiVersion}/containers`,
+        headers: {
+          Authorization: apiKey,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        formData: createFormdataObject(ContainerCreateOptions),
+      };
+      request(options, (error, response) => {
+        if (error) throw reject(new Error(error));
+        logger.info('Create Container Body:');
+        logger.info(response.body);
+        checkForRightResponse(response);
+        resolve(response.body);
+      });
+    }));
+  },
 
-    getConfigDataFromJson: async function (filePath) {
-        let allData = await fs.readFileSync(__dirname + filePath, 'utf8');
-        // console.log("AllData received")
-        return allData;
-    }
-}
+  async getConfigDataFromJson(filePath) {
+    return fs.readFileSync(path.join(__dirname, filePath), 'utf8');
+  },
+};
--- netfieldio.js
+++ netfieldio.js
@@ -1,144 +1,157 @@
-var netfieldio = require('.');
 const { program } = require('commander');
+const pino = require('pino');
+const netfieldio = require('.');
+
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
 
 async function getDeviceConfig(configFile) {
-    let containerOption = '{}';
-    if (configFile != undefined) {
-        containerOption = await netfieldio.getConfigDataFromJson(configFile);
-    };
-    return containerOption;
-};
+  let containerOption = '{}';
+  if (configFile !== undefined) {
+    containerOption = await netfieldio.getConfigDataFromJson(configFile);
+  }
+  return containerOption;
+}
 
 async function getContainerConfig(configFile, tagName) {
-    let configData = await netfieldio.getConfigDataFromJson(configFile);
-    configData = JSON.parse(configData);
-    configData.tagName = tagName;
-    return configData;
+  let configData = await netfieldio.getConfigDataFromJson(configFile);
+  configData = JSON.parse(configData);
+  configData.tagName = tagName;
+  return configData;
 }
 
 
 async function createContainer(apiKey, tagName, containerConfigFile) {
-    console.log('- Release:'.padEnd(20), tagName);
-    console.log('- Config Container:'.padEnd(20), containerConfigFile);
+  logger.info('- Release:'.padEnd(20), tagName);
+  logger.info('- Config Container:'.padEnd(20), containerConfigFile);
 
-    let configData = await getContainerConfig(containerConfigFile, tagName);
+  const configData = await getContainerConfig(containerConfigFile, tagName);
 
-    let responseCreate = await netfieldio.createContainer(apiKey, configData);
-};
+  const responseCreate = await netfieldio.createContainer(apiKey, configData);
+  return responseCreate;
+}
 
 async function createAndDeployContainer(apiKey, tagName, containerConfigFile, deviceConfigFile, deviceId) {
-    console.log('- Release:'.padEnd(20), tagName);
-    console.log('- Device:'.padEnd(20), deviceId);
-    console.log('- Config Container:'.padEnd(20), containerConfigFile);
-    console.log('- Config Device:'.padEnd(20), deviceConfigFile);
+  logger.info('- Release:'.padEnd(20), tagName);
+  logger.info('- Device:'.padEnd(20), deviceId);
+  logger.info('- Config Container:'.padEnd(20), containerConfigFile);
+  logger.info('- Config Device:'.padEnd(20), deviceConfigFile);
 
-    let configData = await getContainerConfig(containerConfigFile, tagName);
-    let containerOption = await getDeviceConfig(deviceConfigFile);
+  const configData = await getContainerConfig(containerConfigFile, tagName);
+  const containerOption = await getDeviceConfig(deviceConfigFile);
 
-    let responseCreate = await netfieldio.createContainer(apiKey, configData);
-    responseCreate = JSON.parse(responseCreate);
-    containerId = responseCreate.id;
-    let responseCreateDevice = await netfieldio.createDeviceContainer(apiKey, deviceId, containerId, containerOption);
-};
+  let responseCreate = await netfieldio.createContainer(apiKey, configData);
+  responseCreate = JSON.parse(responseCreate);
+  const containerId = responseCreate.id;
+  const responseCreateDevice = await netfieldio.createDeviceContainer(apiKey, deviceId, containerId, containerOption);
+
+  return responseCreateDevice;
+}
 
 async function deployContainer(apiKey, deviceId, containerId, deviceConfigFile) {
-    console.log('- Device:'.padEnd(20), deviceId);
-    console.log('- Container:'.padEnd(20), containerId);
-    console.log('- Config Path:'.padEnd(20), deviceConfigFile);
+  logger.info('- Device:'.padEnd(20), deviceId);
+  logger.info('- Container:'.padEnd(20), containerId);
+  logger.info('- Config Path:'.padEnd(20), deviceConfigFile);
 
-    let containerOption = await getDeviceConfig(deviceConfigFile);
+  const containerOption = await getDeviceConfig(deviceConfigFile);
 
-    let responseCreate = await netfieldio.createDeviceContainer(apiKey, deviceId, containerId, containerOption);
-};
+  const responseCreate = await netfieldio.createDeviceContainer(apiKey, deviceId, containerId, containerOption);
+
+  return responseCreate;
+}
 
 async function updateAndRedeployContainer(apiKey, deviceId, containerId, tagName, containerConfigFile, deviceConfigFile) {
-    console.log('- Release:'.padEnd(20), tagName);
-    console.log('- Device:'.padEnd(20), deviceId);
-    console.log('- Container:'.padEnd(20), containerId);
-    console.log('- Config Container:'.padEnd(20), containerConfigFile);
-    console.log('- Config Device:'.padEnd(20), deviceConfigFile);
+  logger.info('- Release:'.padEnd(20), tagName);
+  logger.info('- Device:'.padEnd(20), deviceId);
+  logger.info('- Container:'.padEnd(20), containerId);
+  logger.info('- Config Container:'.padEnd(20), containerConfigFile);
+  logger.info('- Config Device:'.padEnd(20), deviceConfigFile);
 
-    let containerOption = await getDeviceConfig(deviceConfigFile);
-    let configData = await getContainerConfig(containerConfigFile, tagName);
+  const containerOption = await getDeviceConfig(deviceConfigFile);
+  const configData = await getContainerConfig(containerConfigFile, tagName);
 
-    let responseUpdate = await netfieldio.updateContainer(apiKey, containerId, configData);
-    let responseDelete = await netfieldio.deleteDeviceContainer(apiKey, deviceId, containerId);
-    let responseCreate = await netfieldio.createDeviceContainer(apiKey, deviceId, containerId, containerOption);
-};
+  const responseUpdate = await netfieldio.updateContainer(apiKey, containerId, configData);
+  const responseDelete = await netfieldio.deleteDeviceContainer(apiKey, deviceId, containerId);
+  const responseCreate = await netfieldio.createDeviceContainer(apiKey, deviceId, containerId, containerOption);
 
-
-program
-    .version('0.0.1');
+  return [responseUpdate, responseDelete, responseCreate];
+}
 
 program
-    .command('createContainer')
-    .alias('cc')
-    .description('Create a container on the gateway')
-    .requiredOption('-k, --key <key>', 'api key from netfieldio')
-    .requiredOption('-t, --tag <string>', 'version tag of the image')
-    .requiredOption('-o, --config <path>', 'path to the config.JSON file')
-    .action(function (options) {
-        // console.log(options);
-        createContainer(options.key, options.tag, options.config);
-    }).on('--help', function () {
-        console.log('\nNeed the apikey, tag of the image, and a JSON file with all the container options.');
-        console.log('Please refer to the docs or the official netfieldio API for the structure of the JSON.');
-        console.log('Not needed values should left blank and must not be deleted!\n');
-    });
+  .version('0.0.1');
 
 program
-    .command('createAndDeployContainer')
-    .alias('cdc')
-    .description('Create a container on the gateway and deploy it to the given device')
-    .requiredOption('-k, --key <key>', 'api key from netfieldio')
-    .requiredOption('-t, --tag <string>', 'version tag of the image')
-    .requiredOption('-d, --device <id>', 'device id of the device to deploy to')
-    .requiredOption('-oc, --config-container <path>', 'path to the config.JSON for the container, identical to the create one')
-    .option('-od, --config-device <path>', 'path to the config.JSON for the device, can be empty')
-    .action(function (options) {
-        // console.log(options);
-        createAndDeployContainer(options.key, options.tag, options.configContainer, options.configDevice, options.device);
-    }).on('--help', function () {
-        console.log('\nNeed the apikey, the device id, tag of the image, and a JSON file with all the container options.');
-        console.log('Please refer to the docs or the official netfieldio API for the structure of the JSON.');
-        console.log('Not needed values should left blank and must not be deleted!');
-        console.log('The device config only contains values wich differ from standard container config.\n');
-    });
+  .command('createContainer')
+  .alias('cc')
+  .description('Create a container on the gateway')
+  .requiredOption('-k, --key <key>', 'api key from netfieldio')
+  .requiredOption('-t, --tag <string>', 'version tag of the image')
+  .requiredOption('-o, --config <path>', 'path to the config.JSON file')
+  .action((options) => {
+    // logger.info(options);
+    createContainer(options.key, options.tag, options.config);
+  })
+  .on('--help', () => {
+    logger.info('\nNeed the apikey, tag of the image, and a JSON file with all the container options.');
+    logger.info('Please refer to the docs or the official netfieldio API for the structure of the JSON.');
+    logger.info('Not needed values should left blank and must not be deleted!\n');
+  });
 
 program
-    .command('deployContainer')
-    .alias('dc')
-    .description('Deploy an existing container to a given device')
-    .requiredOption('-k, --key <key>', 'api key from netfieldio')
-    .requiredOption('-c, --container <id>', 'container id of the container to update')
-    .requiredOption('-d, --device <id>', 'device id of the device to redeploy to')
-    .option('-o, --config <path>', 'path to the config.JSON, only if other parameters than default container options')
-    .action(function (options) {
-        // console.log(options);
-        deployContainer(options.key, options.device, options.container, options.config);
-    }).on('--help', function () {
-        console.log('\nNeed the apikey, id of the container and the device.');
-        console.log('Please refer to the docs or the official netfieldio API for the structure of the JSON. Only contains values wich differ from standard container config.\n');
-    });
+  .command('createAndDeployContainer')
+  .alias('cdc')
+  .description('Create a container on the gateway and deploy it to the given device')
+  .requiredOption('-k, --key <key>', 'api key from netfieldio')
+  .requiredOption('-t, --tag <string>', 'version tag of the image')
+  .requiredOption('-d, --device <id>', 'device id of the device to deploy to')
+  .requiredOption('-oc, --config-container <path>', 'path to the config.JSON for the container, identical to the create one')
+  .option('-od, --config-device <path>', 'path to the config.JSON for the device, can be empty')
+  .action((options) => {
+    // logger.info(options);
+    createAndDeployContainer(options.key, options.tag, options.configContainer, options.configDevice, options.device);
+  })
+  .on('--help', () => {
+    logger.info('\nNeed the apikey, the device id, tag of the image, and a JSON file with all the container options.');
+    logger.info('Please refer to the docs or the official netfieldio API for the structure of the JSON.');
+    logger.info('Not needed values should left blank and must not be deleted!');
+    logger.info('The device config only contains values wich differ from standard container config.\n');
+  });
 
 program
-    .command('updateAndRedeployContainer')
-    .alias('udc')
-    .description('Update an existing container, delete and redeploy it to a given device')
-    .requiredOption('-k, --key <key>', 'api key from netfieldio')
-    .requiredOption('-t, --tag <string>', 'version tag of the image')
-    .requiredOption('-c, --container <id>', 'container id of the container to update')
-    .requiredOption('-d, --device <id>', 'device id of the device to redeploy to')
-    .requiredOption('-oc, --config-container <path>', 'path to the config.JSON for the container, identical to the create one')
-    .option('-od, --config-device <path>', 'path to the config.JSON for the device, can be empty')
-    .action(function (options) {
-        // console.log(options);
-        updateAndRedeployContainer(options.key, options.device, options.container, options.tag, options.configContainer, options.configDevice);
-    }).on('--help', function () {
-        console.log('\nNeed the apikey, url and tag of the image, id of the container and the device.');
-        console.log('Please refer to the docs or the official netfieldio API for the structure of the JSON.');
-        console.log('Not needed values should left blank and must not be deleted in the containerconfig!');
-        console.log('The device config only contains values wich differ from standard container config.\n');
-    });
+  .command('deployContainer')
+  .alias('dc')
+  .description('Deploy an existing container to a given device')
+  .requiredOption('-k, --key <key>', 'api key from netfieldio')
+  .requiredOption('-c, --container <id>', 'container id of the container to update')
+  .requiredOption('-d, --device <id>', 'device id of the device to redeploy to')
+  .option('-o, --config <path>', 'path to the config.JSON, only if other parameters than default container options')
+  .action((options) => {
+    // logger.info(options);
+    deployContainer(options.key, options.device, options.container, options.config);
+  })
+  .on('--help', () => {
+    logger.info('\nNeed the apikey, id of the container and the device.');
+    logger.info('Please refer to the docs or the official netfieldio API for the structure of the JSON. Only contains values wich differ from standard container config.\n');
+  });
+
+program
+  .command('updateAndRedeployContainer')
+  .alias('udc')
+  .description('Update an existing container, delete and redeploy it to a given device')
+  .requiredOption('-k, --key <key>', 'api key from netfieldio')
+  .requiredOption('-t, --tag <string>', 'version tag of the image')
+  .requiredOption('-c, --container <id>', 'container id of the container to update')
+  .requiredOption('-d, --device <id>', 'device id of the device to redeploy to')
+  .requiredOption('-oc, --config-container <path>', 'path to the config.JSON for the container, identical to the create one')
+  .option('-od, --config-device <path>', 'path to the config.JSON for the device, can be empty')
+  .action((options) => {
+    // logger.info(options);
+    updateAndRedeployContainer(options.key, options.device, options.container, options.tag, options.configContainer, options.configDevice);
+  })
+  .on('--help', () => {
+    logger.info('\nNeed the apikey, url and tag of the image, id of the container and the device.');
+    logger.info('Please refer to the docs or the official netfieldio API for the structure of the JSON.');
+    logger.info('Not needed values should left blank and must not be deleted in the containerconfig!');
+    logger.info('The device config only contains values wich differ from standard container config.\n');
+  });
 
 program.parse(process.argv);
